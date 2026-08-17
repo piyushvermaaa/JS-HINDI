c30de87e017357faeb8eff40aa0c4fcdeb76b159
Piyush Verma <vpiyush2278@gmail.com>
2026-08-17 19:53:39 +0000

    types of data types

---
+++ 01_basics/datatypes.summary.js
@@ -0,0 +1,39 @@
+// Primitive 
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId)
+// false
+
+const bigInt = 3244606493703779n
+
+
+// Reference (Non primitve)
+
+// Array, Objects, Functions
+// Array
+const heros = ["shaktiman", "nagraj", "doga"]
+// Object
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+// Object
+const myFunction = function(){
+    console.log("hello word")
+}
+
+console.log(typeof bigInt)
+console.log(typeof myFunction)
+// Function
+
